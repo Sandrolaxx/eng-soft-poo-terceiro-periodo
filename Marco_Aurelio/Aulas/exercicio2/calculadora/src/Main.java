77e4e8158feca2feda75d7b923ecec0faab5996b
Marco Aurélio Kovaleski <marco.kovaleski18@gmail.com>
2023-08-26 16:56:22 -0300

    commit calc atualizada

--- Marco_Aurelio/Aulas/exercicio2/calculadora/src/Main.java
+++ Marco_Aurelio/Aulas/exercicio2/calculadora/src/Main.java
@@ -7,41 +7,57 @@ public class Main {
 
         int escolha = 0;
 
-        while (escolha != 5){
-        System.out.println("Selecione uma operação: \n[1] SOMA \n[2] SUBTRAÇÃO \n[3] MUTIPLICAÇÃO \n[4] DIVISÃO \n[5] SAIR");
-         escolha = scanner.nextInt();
+        while (escolha != 5) {
+            System.out.println(
+                    "Selecione uma operação: \n[1] SOMA \n[2] SUBTRAÇÃO \n[3] MUTIPLICAÇÃO \n[4] DIVISÃO \n[5] SAIR");
+            escolha = scanner.nextInt();
 
-        System.out.println("Digite seus valores: ");
-        double v1 = scanner.nextDouble();
-        System.out.println("+ - * /");
-        double v2 = scanner.nextDouble();
+            double resultado = 0;
 
-        double resultado = 0;
+            switch (escolha) {
+                case 1:
+                    System.out.println("Digite seus valores: ");
+                    double v1 = scanner.nextDouble();
+                    System.out.println("+");
+                    double v2 = scanner.nextDouble();
+                    resultado = Calculadora.soma(v1, v2);
+                    break;
 
-        switch (escolha) {
-            case 1:
-                resultado = Calculadora.soma(v1, v2);
-            break;
+                case 2:
+                    System.out.println("Digite seus valores: ");
+                    v1 = scanner.nextDouble();
+                    System.out.println("-");
+                    v2 = scanner.nextDouble();
+                    resultado = Calculadora.subt(v1, v2);
+                    break;
 
-            case 2:
-                resultado = Calculadora.subt(v1, v2);
-            break;
+                case 3:
+                    System.out.println("Digite seus valores: ");
+                    v1 = scanner.nextDouble();
+                    System.out.println("*");
+                    v2 = scanner.nextDouble();
+                    resultado = Calculadora.multi(v1, v2);
+                    break;
 
-            case 3:
-                resultado = Calculadora.multi(v1, v2);
-            break;
+                case 4:
+                    System.out.println("Digite seus valores: ");
+                    v1 = scanner.nextDouble();
+                    System.out.println("/");
+                    v2 = scanner.nextDouble();
+                    resultado = Calculadora.divi(v1, v2);
+                    break;
 
-            case 4:
-                resultado = Calculadora.divi(v1, v2);
-            break;
+                default:
+                    if (escolha == 5) {
+                        System.out.println("...");
+                    } else {
+                        System.out.println("Operação invalida! ");
+                    }
+                    break;
+            }
 
-            default:
-                System.out.println("Operação invalida! ");
-            break;
+            System.out.println("Resultado: " + resultado);
         }
-
-        System.out.println("Resultado: " + resultado);
-    }
 
         scanner.close();
     }
