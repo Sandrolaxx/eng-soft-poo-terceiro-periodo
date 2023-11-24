291cfa6daca06bccfb8649e1812312fe65d1d0ca
unknown <lgdsantos@minha.fag.edu.br>
2023-11-24 20:20:39 -0300

    Trabalho bala na agulha

---
+++ Luciano_Goulart/2ºBimestre/payment-app/payment-app/src/main/java/com/fag/RestClientCelcoin.java
@@ -0,0 +1,31 @@
+package com.fag;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import com.fag.dto.ConsultarBoletoDTO;
+import com.fag.dto.TokenDTO;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Form;
+import jakarta.ws.rs.core.MediaType;
+
+@RegisterRestClient(baseUri = "https://sandbox.openfinance.celcoin.dev")
+public interface RestClientCelcoin {
+
+    @POST
+    @Path("/v5/token")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    TokenDTO getToken(Form form);
+
+    @POST
+    @Path("/v5/transactions/billpayments/authorize")
+    String consult(@HeaderParam("Authorization") String token, ConsultarBoletoDTO dto);
+
+    @POST
+    @Path("/v5/transactions/billpayments")
+    String payment(@HeaderParam("Authorization") String token, ConsultarBoletoDTO dto);
+    
+}
